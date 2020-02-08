cf6844259dd8c6e4c4af2a95ad3a00c79b42ab13
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-02-08 23:20:40 +0100

    test: add cypress test

--- apps/component-harnesses-jest-cypress-e2e/src/integration/app.spec.ts
+++ apps/component-harnesses-jest-cypress-e2e/src/integration/app.spec.ts
@@ -1,5 +1,3 @@
-import { getGreeting } from '../support/app.po';
-
 describe('component-harnesses-jest-cypress', () => {
   beforeEach(() => cy.visit('/'));
 
@@ -7,7 +5,14 @@ describe('component-harnesses-jest-cypress', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to component-harnesses-jest-cypress!');
+    cy.get('[cy-data="weekday-picker"] .mat-form-field-flex').click();
+
+    cy.get('[cy-data="Friday-option"]').as('fridayOption');
+
+    cy.get('@fridayOption').find('.mat-option-text').should('contain.text', 'Friday');
+
+    cy.get('@fridayOption').click();
+
+    cy.get('.mat-select-value-text span').should('contain.text', 'Friday');
   });
 });
